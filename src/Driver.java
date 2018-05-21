c7377aa4a56c0d19301b46415e846b95481b2025
José Victor Viriato <jvviriato@inf.ufsm.br>
2018-05-21 17:09:24 -0300

    acho q foi

--- src/Driver.java
+++ src/Driver.java
@@ -1,14 +1,15 @@
 
 import iterator.Iterator;
 import iterator.ExtratorTXT;
+import iterator.ExtratorXML;
 import java.io.IOException;
 
 public class Driver {
         public static void main(String[] args) throws IOException {
             if (args.length == 2){
-                Iterator it = new Iterator();
                 ExtratorTXT txt = new ExtratorTXT();
-//                it.open(args[0]);
+                ExtratorXML xml = new ExtratorXML();
+                xml.open(args[0]);
                 txt.open(args[1]);
 
             }
--- src/iterator/Iterator.java
+++ src/iterator/Iterator.java
@@ -7,7 +7,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import registro.Registro;
 
-public class Iterator {
+public abstract class Iterator {
     public List<Registro> regs = new ArrayList<>();
     public void open(String path) throws FileNotFoundException, IOException{
         try(RandomAccessFile raf = new RandomAccessFile(path, "r")){
@@ -19,75 +19,29 @@ public class Iterator {
         
     }
 
-    public Registro next(RandomAccessFile raf) throws IOException{
-        String linha = "";
-        String regex_registro_inicio = "^([' ']*(<registro>))";
-        String regex_registro_fim = "^([' ']*(</registro>))";
-        String regex_campos = "^([' ']*<[\\w]*>)([\\w]*)(</[\\w]*>)";
-        Registro reg = new Registro();
-        boolean dentro_do_registro = false;
-        try {
-            while ((linha = raf.readLine()) != null) {
-                if(linha.matches(regex_registro_inicio)){
-                    dentro_do_registro = true;
-                }
-                else if(linha.matches(regex_registro_fim)){
-                    dentro_do_registro = false;
-                    break;
-                }
-                else if(dentro_do_registro){
-                    Pattern pattern = Pattern.compile(regex_campos);
-                    Matcher matcher = pattern.matcher(linha.trim());
-                    while(matcher.find()){
-                        trataRegistro(matcher.group(1), matcher.group(2), matcher.group(3), reg);
-                    }
-                }
+    public abstract Registro next(RandomAccessFile raf) throws IOException;
+    public abstract boolean hasNext(RandomAccessFile raf) throws IOException;
+    public void printaCampos()
+    {
+        for(Registro r : regs){
+            for(int i = 0; i < r.getFieldCount(); i++){
+                System.out.println(r.getFieldName(i) + ": " + r.getFieldValue(i));
             }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        return reg;
-    }
-    
-    public void trataRegistro(String campoInit, String dado, String campoFim, Registro reg){
-        if(registroEhValido(campoInit, campoFim)){
-            reg.addReg(campoInit.replaceAll("[<>]", ""), dado);
-        }else{
-            System.out.println("Campo inválido!");
+            System.out.println("");
         }
-    }
-    
-    public boolean registroEhValido(String campoInit, String campoFim){
-        return campoInit.equals(campoFim.replace("/", ""));
+        
     }
     public void run(RandomAccessFile raf) throws IOException{
         Registro reg = next(raf);
-        reg.printaCampos();
-
+        regs.add(reg);
         while(hasNext(raf)){
             reg = next(raf);
-            reg.printaCampos();
+            regs.add(reg);
         }
+        printaCampos();
     }
 
-    public boolean hasNext(RandomAccessFile raf) throws IOException{
-        long fp = raf.getFilePointer();
-        String linha = "";
-        String regex_registro_inicio = "^([' ']*(<registro>))";
-        
-        try {
-            while ((linha = raf.readLine()) != null) {
-                if(linha.matches(regex_registro_inicio)){
-                    raf.seek(fp);
-                    return true;
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        raf.seek(fp);
-        return false;
-    }
+
     public void close(RandomAccessFile raf) throws IOException{
         raf.close();
     }
